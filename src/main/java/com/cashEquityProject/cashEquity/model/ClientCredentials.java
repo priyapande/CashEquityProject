abaed7276f0d447b364608794c596901ca4ffba1
Ankit Bindal <ankitb.1997@gmail.com>
2018-07-25 12:07:31 +0530

    Added Client Credentials model

--- src/main/java/com/cashEquityProject/cashEquity/model/ClientCredentials.java
+++ src/main/java/com/cashEquityProject/cashEquity/model/ClientCredentials.java
@@ -1,4 +1,10 @@
 package com.cashEquityProject.cashEquity.model;
 
 public class ClientCredentials {
+
+    private String clientCode, password;
+
+    public String getClientCode() {
+        return clientCode;
+    }
 }
