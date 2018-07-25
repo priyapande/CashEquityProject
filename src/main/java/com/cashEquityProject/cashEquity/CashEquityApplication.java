b7772c6f34cb56c306db4bb82716c5ab0bff0900
priyapande <priyapande07@gmail.com>
2018-07-25 11:04:39 +0530

    basic structure

---
+++ src/main/java/com/cashEquityProject/cashEquity/CashEquityApplication.java
@@ -0,0 +1,12 @@
+package com.cashEquityProject.cashEquity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashEquityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashEquityApplication.class, args);
+	}
+}
