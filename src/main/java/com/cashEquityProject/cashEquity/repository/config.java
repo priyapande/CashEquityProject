f09ba22fe5f3790d88f9a943980b7812972ec402
Ankit Bindal <ankitb.1997@gmail.com>
2018-07-25 14:33:50 +0530

    Added confi file

---
+++ src/main/java/com/cashEquityProject/cashEquity/repository/config.java
@@ -0,0 +1,8 @@
+package com.cashEquityProject.cashEquity.repository;
+
+public interface config {
+
+    final public static Integer SUCCESS = 200;
+    final public static Integer FAILED = 400;
+
+}
