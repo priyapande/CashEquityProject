3eb47cdcf38205d26d88857978a601db70bf0023
srishtichadha16 <srishtichadha16@gmail.com>
2018-07-30 09:48:22 +0530

    Adding constants to config

--- src/main/java/com/cashEquityProject/cashEquity/repository/config.java
+++ src/main/java/com/cashEquityProject/cashEquity/repository/config.java
@@ -8,4 +8,14 @@ public interface config {
     final public static Integer INVALID_USER = 500;
     final public static Integer INVALID_PASSWORD = 600;
 
+    // Order status Information
+    // 0 - Not yet executed
+    // 1 - In progress
+    // 2 - Order executed
+
+    final public static Integer NOT_EXECUTED = 0;
+    final public static Integer IN_PROGRESS = 1;
+    final public static Integer EXECUTED = 2;
+
+
 }
