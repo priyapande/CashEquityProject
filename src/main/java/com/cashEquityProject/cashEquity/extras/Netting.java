43dc86894e7f2234d91187ea606a9a9bc19e4add
Ankit Bindal <ankitb.1997@gmail.com>
2018-07-31 15:30:03 +0530

    Fixed (price, time) ordering

--- src/main/java/com/cashEquityProject/cashEquity/extras/Netting.java
+++ src/main/java/com/cashEquityProject/cashEquity/extras/Netting.java
@@ -6,7 +6,9 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Repository
 public class Netting implements Runnable{
@@ -15,6 +17,7 @@ public class Netting implements Runnable{
     JdbcTemplate jdbcTemplate;
 
     private Order order;
+    Logger logger = Logger.getLogger(Netting.class.getName());
 
     public Netting() {}
 
@@ -25,10 +28,15 @@ public class Netting implements Runnable{
     @Override
     public void run() {
 
+        logger.info("Inside Netting run() function");
+
         System.out.println(order.toString());
 
-        String sqlBuy = "select * from orders where orderstatus in (0,1) and direction='S' and symbol=? and orderid!=? order by limitprice DESC, tradetime ASC";
-        String sqlSell = "select * from orders where orderstatus in (0,1) and direction='B' and symbol=? and orderid!=? order by limitprice ASC, tradetime ASC";
+        // String sqlBuy = "select * from orders where orderstatus in (0,1) and direction='S' and symbol=? and orderid!=? order by limitprice DESC, tradetime ASC";
+        // String sqlSell = "select * from orders where orderstatus in (0,1) and direction='B' and symbol=? and orderid!=? order by limitprice ASC, tradetime ASC";
+
+        String sqlBuy = "select * from orders where orderstatus in (0,1) and direction='S' and symbol=? and orderid!=?";
+        String sqlSell = "select * from orders where orderstatus in (0,1) and direction='B' and symbol=? and orderid!=?";
 
         List<Order> buyNetList = jdbcTemplate.query(sqlBuy,
                 new Object[]{order.getSymbol(), order.getOrderId()},
@@ -37,6 +45,39 @@ public class Netting implements Runnable{
         List<Order> sellNetList = jdbcTemplate.query(sqlSell,
                 new Object[]{order.getSymbol(), order.getOrderId()},
                 new BeanPropertyRowMapper<>(Order.class));
+
+        // limitprice DESC, tradeprice ASC
+        buyNetList.sort(new Comparator<Order>() {
+
+            @Override
+            public int compare(Order o1, Order o2) {
+
+                // o2 first because we want to sort in DESC order
+                int cmp1 = o2.getLimitPrice().compareTo(o1.getLimitPrice());
+
+                if (cmp1 == 0) {
+                    return TimeComparator.compare(o1.getTradetime(), o2.getTradetime());
+                }
+
+                return cmp1;
+            }
+        });
+
+        // limitprice ASC, tradeprice ASC
+        sellNetList.sort(new Comparator<Order>() {
+
+            @Override
+            public int compare(Order o1, Order o2) {
+
+                int cmp1 = o1.getLimitPrice().compareTo(o2.getLimitPrice());
+
+                if (cmp1 == 0) {
+                    return TimeComparator.compare(o1.getTradetime(), o2.getTradetime());
+                }
+
+                return cmp1;
+            }
+        });
 
         // BUY ORDER
         for(Order pastOrderBuy: buyNetList) {
