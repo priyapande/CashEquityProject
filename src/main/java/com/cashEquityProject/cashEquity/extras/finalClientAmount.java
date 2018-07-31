4f29eb95e47f12c72042b4ef5bfbed185d2a078d
samikshamainali <samisharma999@gmail.com>
2018-07-31 16:43:30 +0530

    "altered the finalclientamount, added quantity of shares processed"

--- src/main/java/com/cashEquityProject/cashEquity/extras/finalClientAmount.java
+++ src/main/java/com/cashEquityProject/cashEquity/extras/finalClientAmount.java
@@ -7,39 +7,56 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 
+class pck
+{
+    public double payablePck;
+    public int remainingQuantityPck;
+    public double receivablePck;
+}
+
 public class finalClientAmount {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+
     public double receivable=0;
     public double payable=0;
+    public int remainingQuantity=0;
 
-    public Double clientNetPayable(String code) {
+    public pck clientNetPayable(String code) {
 
         String sqlBuy = "select value from orders where direction='B' and clientCode=?";
 
+        pck objBuy=new pck();
         List<Order> clientPayable = jdbcTemplate.query(sqlBuy,
                 new Object[]{code},
                 new BeanPropertyRowMapper<>(Order.class));
         for(Order element : clientPayable)
         {
             payable=payable + element.getLimitPrice()*(element.getQuantity()-element.getRemainingquantity());
+            remainingQuantity=remainingQuantity+element.getQuantity()-element.getRemainingquantity();
         }
-        return payable;
+        objBuy.payablePck=payable;
+        objBuy.remainingQuantityPck=remainingQuantity;
+        return objBuy;
     }
 
-    public Double clientNetReceivable(String symbol) {
+    public pck clientNetReceivable(String symbol) {
 
 
         String sqlSell = "select value from orders where direction='S' and clientCode=?";
 
+        pck objSell=new pck();
         List<Order> clientReceivable = jdbcTemplate.query(sqlSell,
                 new Object[]{symbol},
                 new BeanPropertyRowMapper<>(Order.class));
         for(Order element : clientReceivable)
         {
             receivable=receivable + element.getLimitPrice()*(element.getQuantity()-element.getRemainingquantity());
+            remainingQuantity=remainingQuantity+element.getQuantity()-element.getRemainingquantity();
         }
-        return receivable;
+        objSell.receivablePck=receivable;
+        objSell.remainingQuantityPck=remainingQuantity;
+        return objSell;
     }
 }
